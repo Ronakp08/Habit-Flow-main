e27487c31f96c788b7d0636ee6705f385363ca68
Ronakp08 <ronakp6393@gmail.com>
2026-08-18 22:59:39 +0530

    updated code

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -12,8 +12,14 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD || "",
   {
     host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
     dialect: "mysql",
     logging: false,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: true,
+      },
+    },
   }
 );
 
